804ce64b2a8d59dcfc95de9c60c2f7f420e26d70
TianYou <tianyou0714@qq.com>
2026-01-16 15:49:13 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -84,7 +84,7 @@ public class EmployeeController {
      */
     @PostMapping
     @ApiOperation("新增员工")
-    public Result<String>  save(@RequestBody EmployeeDTO employeeDTO) {
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
         log.info("新增员工：{}", employeeDTO);
         System.out.println("当前线程的id" + Thread.currentThread().getId());
         employeeService.save(employeeDTO);
